1e13af4797032b4b2e776f6120003d0b2123ff5f
decoder <piotrzan@gmail.com>
2019-12-04 22:15:56 +0100

    sample playbook

--- 1-install-ansible.sh
+++ 1-install-ansible.sh
@@ -1,4 +1,6 @@
 
+sudo apt-get update
+
 sudo apt-get -y install python-pip python-dev libffi-dev libssl-dev
 
 pip install ansible --user
@@ -6,4 +8,4 @@ pip install ansible --user
 echo 'PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 echo 'PATH=$HOME/.local/bin:$PATH' >> ~/.zshrc
 
-source .zshrc
+source ~/.zshrc
